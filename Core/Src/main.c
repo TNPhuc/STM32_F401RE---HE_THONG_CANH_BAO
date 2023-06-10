4ea76c6b79aa9e2a815c398ada6321228095573f
TNPhuc <kiemgaykaka@gmail.com>
2023-06-10 22:28:49 +0700

    STM32-F401RE

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,18 +1,15 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
+  *
   * @file           : main.c
   * @brief          : Main program body
+  *
   ******************************************************************************
-  * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
+  * PROJECT: HE THONG CANH BAO SU DUNG STM32 F401RE
+  * Chu tri du an: TRAN NGUYEN PHUC
+  * Thuc hien: TRAN NGUYEN PHUC - VO THỊ HONG ANH - PHAM DANG KIEU OANH
   *
   ******************************************************************************
   */
